63a36207f40de4feeefae94a705d0910aa0a31f3
Luis Felipe Vidal Arellano <d805664@SF.PREFEITURA.SP.GOV.BR>
2019-02-22 12:48:20 -0300

    Inclusao do calculo do Beta

--- stocks.py
+++ stocks.py
@@ -10,14 +10,18 @@ import requests
 import bs4
 import time, datetime
 import matplotlib.pyplot as plt
+from sklearn import linear_model
+import numpy as np
 
 plt.style.use("ggplot")
 
-data_inicial = round(time.mktime((datetime.datetime(2010,11,1)).timetuple()))
+## Extração dos dados do ativo.
+
+data_inicial = round(time.mktime((datetime.datetime(2013,11,1)).timetuple()))
 data_final = round(time.mktime((datetime.datetime(2019,2,18)).timetuple()))
 intervalo = "1mo" #options: 1d 1wk 1mo
 frequencia = "1mo" #options: 1d 1wk 1mo
-ticker = "BRML3"
+ticker = "PETR4"
 
 
 url = "https://finance.yahoo.com/quote/{4}.SA/history?period1={0}&period2={1}&interval={2}&filter=history&frequency={3}".format(data_inicial,data_final, intervalo, frequencia, ticker)
@@ -26,17 +30,64 @@ html = requests.get(url)
 
 soup = bs4.BeautifulSoup(html.text, "lxml")
 
-df = pd.read_html((soup.table).prettify())[0]
-df.dropna(inplace=True)
+petr = pd.read_html((soup.table).prettify())[0]
+petr.dropna(inplace=True)
 
-df["Date"] = pd.to_datetime(df["Date"])
+petr["Date"] = pd.to_datetime(petr["Date"])
 
-df.set_index("Date", inplace=True)
-df.sort_index(inplace=True)
+petr.set_index("Date", inplace=True)
+petr.sort_index(inplace=True)
 
-df["Close30"] = df["Close*"].rolling(30).mean()
-df["Close10"] = df["Close*"].rolling(10).mean()
-df["Pct_Change"] = df["Close*"].pct_change()
+petr["Close30"] = petr["Close*"].rolling(30).mean()
+petr["Close10"] = petr["Close*"].rolling(10).mean()
+petr["Pct_Change"] = petr["Close*"].pct_change()
 
-df[["Close*","Pct_Change"]].plot(subplots=True, title=ticker, layout=(2,1), color="br")
 
+## Extração dos dados do IBOVESPA.
+
+data_inicial = round(time.mktime((datetime.datetime(2013,11,1)).timetuple()))
+data_final = round(time.mktime((datetime.datetime(2019,2,18)).timetuple()))
+intervalo = "1mo" #options: 1d 1wk 1mo
+frequencia = "1mo" #options: 1d 1wk 1mo
+ticker = "%5EBVSP"
+
+
+url = "https://finance.yahoo.com/quote/{4}/history?period1={0}&period2={1}&interval={2}&filter=history&frequency={3}".format(data_inicial,data_final, intervalo, frequencia, ticker)
+
+html = requests.get(url)
+
+soup = bs4.BeautifulSoup(html.text, "lxml")
+
+ibov = pd.read_html((soup.table).prettify())[0]
+ibov.dropna(inplace=True)
+
+ibov["Date"] = pd.to_datetime(ibov["Date"])
+
+ibov.set_index("Date", inplace=True)
+ibov.sort_index(inplace=True)
+
+ibov["Close30"] = ibov["Close*"].rolling(30).mean()
+ibov["Close10"] = ibov["Close*"].rolling(10).mean()
+ibov["Pct_Change"] = ibov["Close*"].pct_change()
+
+
+## Cálculo da correlação
+
+corr = petr["Pct_Change"].corr(ibov["Pct_Change"])
+print("Correlation: {0:.4f}".format(corr))
+
+## Plot das séries
+
+plt.scatter(ibov["Pct_Change"], petr["Pct_Change"])
+
+## Cálculo do beta
+
+X = ibov[["Pct_Change"]]
+Y = petr["Pct_Change"]
+
+model = linear_model.LinearRegression()
+res = model.fit(X[1:],Y[1:].to_frame())
+print("Beta: {0:.4f}\nIntercept: {1:.4f}\nR2:{2:.4f}".format(res.coef_[0][0], res.intercept_[0], model.score(X[1:],Y[1:])))
+
+plt.plot(np.linspace(-.15,.2,100), (np.vectorize(lambda x: res.coef_*x + res.intercept_))(np.linspace(-.15,.2,100)))
+plt.show()
